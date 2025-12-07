4653fad1b2cc25548836a95c959bb5854fb6271b
Illuri charles <illuricharles6@gmail.com>
2025-12-07 09:06:32 +0530

    fix: update ProtectedRoute to use Navigate for clean redirects

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,10 @@
 
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import Loader from "./Loader";
 
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-    const navigate = useNavigate()
   const { isAuthenticated, loading } = useAuth();
 
   if (loading) {
@@ -13,8 +12,7 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
   }
 
   if (!isAuthenticated) {
-    navigate('/login', {replace: true})
-    return null
+    return <Navigate to="/login" replace />;
   }
 
   return children;
